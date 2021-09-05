0ab6a561622d087d64cc0fb78b742a84ac0c9887
Andy Ron <randy.njfu@gmail.com>
2021-09-05 16:50:03 +0800

    暂时删除不要的

--- Spring-in-Action-5th/ch03/taco-cloud/src/main/java/com/andyron/tacos/data/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.andyron.tacos.data;
-
-import com.andyron.tacos.Order;
-
-public interface OrderRepository {
-
-    Order save(Order order);
-}
--- Spring-in-Action-5th/ch03/taco-cloud/src/main/java/com/andyron/tacos/data/TacoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.andyron.tacos.data;
-
-import com.andyron.tacos.Taco;
-
-public interface TacoRepository {
-
-    Taco save(Taco design);
-}
